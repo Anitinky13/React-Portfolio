cbf107a40f06b30b62723f132c0b74e3b9486423
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-09-12 14:18:16 -0500

    topbar somewhat functional

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import "./topbar.scss";
 import { Person, Email } from "@mui/icons-material";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
@@ -19,7 +19,13 @@ export default function Topbar() {
           </div>
         </div>
 
-        <div className="right"></div>
+        <div className="right">
+          <div className="port" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
