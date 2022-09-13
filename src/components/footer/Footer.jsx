1ec323a5d07620b2feb65799d484ea24737e8092
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-09-13 16:59:01 -0500

    all done, just need to make readme

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,31 +14,29 @@ export default function Footer() {
       >
         <CDBBox display="flex" justifyContent="between" className="flex-wrap">
           <CDBBox alignSelf="center">
-            <a href="/" className="d-flex align-items-center p-0 text-dark">
+            <a
+              href="https://anitinky13.github.io/Portfolio/"
+              className="d-flex align-items-center p-0 text-dark"
+            >
               <span className="ml-3 h5 font-weight-bold">
                 A N A S T A S I Y A ~ L I T V I N O V A
               </span>
             </a>
             <CDBBox className="mt-5" display="flex">
               <CDBBtn flat color="dark" className="p-2">
-                <CDBIcon
-                  fab
-                  icon="github"
-                  href="https://github.com/Anitinky13"
-                />
+                <a href="https://github.com/Anitinky13">
+                  <CDBIcon fab icon="github" />
+                </a>
               </CDBBtn>
               <CDBBtn flat color="dark" className="mx-3 p-2">
-                <CDBIcon fab icon="linkedin" />
-              </CDBBtn>
-              <CDBBtn flat color="dark" className="p-2">
-                <CDBIcon fab icon="instagram" />
+                <a href="https://www.linkedin.com/in/simply-ana/">
+                  <CDBIcon fab icon="linkedin" />
+                </a>
               </CDBBtn>
             </CDBBox>
           </CDBBox>
           <CDBBox>
-            <p className="h5 mb-4" style={{ fontWeight: "600" }}>
-              Devwares
-            </p>
+            <p className="h5 mb-4" style={{ fontWeight: "600" }}></p>
             <CDBBox display="flex" flex="column" style={{ cursor: "pointer" }}>
               <CDBFooterLink href="#intro">About Me</CDBFooterLink>
               <CDBFooterLink href="#portfolio">Portfolio</CDBFooterLink>
