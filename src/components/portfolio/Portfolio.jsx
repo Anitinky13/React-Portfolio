cc54912c7a50bab39ab04c4e4b4af60bc9e5884c
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-09-13 15:12:06 -0500

    added links to my projects portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -24,12 +24,14 @@ export default function Portfolio() {
       </ul>
       <div className="container">
         <div className="item">
-          <img
-            src="assets/event.png"
-            alt="Event Radar imag"
-            width="300"
-            height="300"
-          />
+          <a href="https://wumbo-dot.github.io/EventRadar/">
+            <img
+              src="assets/event.png"
+              alt="Event Radar imag"
+              width="300"
+              height="300"
+            />
+          </a>
           <h3>E V E N T R A D A R </h3>
         </div>
         <div className="item">
@@ -42,39 +44,47 @@ export default function Portfolio() {
           <h3>B A N T R </h3>
         </div>
         <div className="item">
-          <img
-            src="assets/rename.png"
-            alt="password generator"
-            width="300"
-            height="300"
-          />
+          <a href="https://bantr-study.herokuapp.com/">
+            <img
+              src="assets/rename.png"
+              alt="password generator"
+              width="300"
+              height="300"
+            />
+          </a>
           <h3>P A S S W O R D</h3>
         </div>
         <div className="item">
-          <img
-            src="assets/taskmaster.png"
-            alt="taskmaster imag"
-            width="300"
-            height="300"
-          />
+          <a href="https://anitinky13.github.io/Password-Generator/">
+            <img
+              src="assets/taskmaster.png"
+              alt="taskmaster imag"
+              width="300"
+              height="300"
+            />
+          </a>
           <h3>T A S K M A S T E R</h3>
         </div>
         <div className="item">
-          <img
-            src="assets/tasks.png"
-            alt="tasks  imag"
-            width="300"
-            height="300"
-          />
+          <a href="https://anitinky13.github.io/taskmaster-pro/">
+            <img
+              src="assets/tasks.png"
+              alt="tasks  imag"
+              width="300"
+              height="300"
+            />
+          </a>
           <h3>T A S K I N A T O R</h3>
         </div>
         <div className="item">
-          <img
-            src="assets/workday.png"
-            alt="workday imag"
-            width="300"
-            height="300"
-          />
+          <a href="https://anitinky13.github.io/Work-Day-Scheduler/">
+            <img
+              src="assets/workday.png"
+              alt="workday imag"
+              width="300"
+              height="300"
+            />
+          </a>
           <h3>W O R K D A Y </h3>
         </div>
       </div>
