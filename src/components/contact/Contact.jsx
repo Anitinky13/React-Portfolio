e207ff3a7bbb97852fb5297c9ca024c12355e739
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-09-13 14:53:35 -0500

    contact form and responsiveness donee

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,38 @@
-import React from "react";
 import "./contact.scss";
-
+import { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 export default function Contact() {
-  return <div className="contact" id="contact"></div>;
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+    if (!validateEmail(email)) {
+      setErrorMessage("Email or username is invalid");
+      return;
+    }
+    setEmail("");
+  };
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/contact.png" alt="" />
+      </div>
+      <div className="right">
+        <h2>C O N T A C T </h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Email" />
+          <textarea placeholder="Message"></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thank you, I will respond ASAP</span>}
+        </form>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,12 +1,19 @@
 import "./resume.scss";
+
 export default function Resume() {
   return (
-    <div className="resume">
-      <h1>Resume</h1>
-      <ul>
-        <li></li>
-      </ul>
-      <div className="container"></div>
-    </div>
+    <section id="resume" class="resume">
+      <h4 class="section-title-primary-border">R E S U M E</h4>
+      <div class="container">
+        <a href="https://docs.google.com/document/d/1Ryzs-IX0zzlg-0OYU6tm2yHGXVfCqtC4gnuaR3gJ-_A/edit?usp=sharing ">
+          <img
+            src=" assets/resume1.png"
+            alt="resume"
+            width="600"
+            height="700"
+          />
+        </a>
+      </div>
+    </section>
   );
 }
